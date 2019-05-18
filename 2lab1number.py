aa4ef2e36ee5de24bb4f9cf25521b6bf603712d0
lores1337 <47796393+lores1337@users.noreply.github.com>
2019-05-19 00:48:49 +0300

    Delete 2lab1number.py

--- 2lab1number.py
+++
@@ -1,9 +0,0 @@
-s = input()
-q = ""
-for i in s.split(";"):
-    for j in i.split(" "):
-        for k in j.split(","):
-            for p in k.split("."):
-                if (p[len(p) - 2:] == "ов"):
-                    q += p + " "
-print(q)                    
